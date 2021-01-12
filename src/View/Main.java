2e4f9d114473b79ace94954ecb4f94d99b7a2731
Yaimuu <yanis.ouled@gmail.com>
2021-01-12 18:19:05 +0100

    Test elements with JavaFX

--- src/View/Main.java
+++ src/View/Main.java
@@ -14,7 +14,8 @@ import java.io.IOException;
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         try
         {
             // Read file fxml and draw interface.
