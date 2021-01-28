5793c56998b4797b4ccc0c3a8683aaa25573abd6
Yaimuu <yanis.ouled@gmail.com>
2021-01-28 15:34:35 +0100

    Update

--- src/Model/SEIRBorn.java
+++ src/Model/SEIRBorn.java
@@ -2,16 +2,11 @@ package Model;
 
 import java.util.List;
 import java.util.Vector;
-<<<<<<< HEAD
-=======
-
->>>>>>> 5198610d9cd7a523677728fc5f5370ed7ffca15b
 
 public class SEIRBorn extends SEIR
 {
     private Double eta; //Bornrate
     private Double mu;  //Deathrate
-<<<<<<< HEAD
 
     // TODO : SEIR avec gestion des naissances
 
@@ -29,30 +24,6 @@ public class SEIRBorn extends SEIR
         E = res.get(1);
         I = res.get(2);
         R = res.get(3);
-=======
-    protected double S;
-    protected double E;
-    protected double I;
-    protected double R;
-
-    // TODO : SEIR avec gestion des naissances
-
-
-    public Vector<Double> calculateStep()
-    {
-        Vector<Double> res = new Vector<Double>();
-
-        res.set(0, -super.beta * S * I + eta*super.N-mu*S);
-        res.set(1, super.beta * S * I - super.alpha*E - mu*E);
-        res.set(2, super.alpha*E-super.gamma*I -mu*I);
-        res.set(3, super.gamma*I -mu*R);
-
-        //je sais pas si tu veux qu'on change les SIR directement
-        //S = res.get(0);
-        //E = res.get(1);
-        //I = res.get(2);
-        //R = res.get(3);
->>>>>>> 5198610d9cd7a523677728fc5f5370ed7ffca15b
 
         return res;
     }
